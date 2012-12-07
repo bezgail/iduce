44b3301aafad329fb47c468fe736f30aaea3b3c0
Kidane Weldemariam <kyw@iduce.org>
2012-12-07 02:09:53 +0000

    first rails in windows

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iduce::Application.config.secret_token = 'd69f710d4ad8c9bdc1829fbadd02bfa0294759553c0dbf705a132fe52f727d10b5fa414ad6c56a94c8a9c0fda69565873a5595956e480ff97be4b2b1d425cd5f'
